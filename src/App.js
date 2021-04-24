d17c0904e506614eac69fd551d523acae3e842f1
pavitra71994 <pavitrank1@gmail.com>
2021-04-24 16:23:59 +0530

    5 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
 import Login from "./Login/Login";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <Login />
+      <Switch>
+        <Route path="/home" component={Login} />
+      </Switch>
     </div>
   );
 }
--- src/UserCard/UserCard.js
+++ src/UserCard/UserCard.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./UserCard.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import LazyLoad from "react-lazyload";
 
 export default function UserCard(props) {
   const [userName, setUserName] = useState("");
   const [isLoaded, setisLoaded] = useState(false);
+  const [count, setCount] = useState({
+    prev: 0,
+    next: 10
+  });
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -39,19 +42,17 @@ export default function UserCard(props) {
       {userName ? (
         <div>
           {userName.map(item => (
-            <LazyLoad height={200}>
-              <div key={item.id}>
-                <Card className="cardLook">
-                  <Card.Img
-                    variant="top"
-                    src="https://cdn.pixabay.com/photo/2016/03/08/20/03/flag-1244648_960_720.jpg"
-                  />
-                  <Card.Body className="usertitleLook">
-                    <Card.Title>{item.username}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </div>
-            </LazyLoad>
+            <div key={item.id}>
+              <Card className="cardLook">
+                <Card.Img
+                  variant="top"
+                  src="https://cdn.pixabay.com/photo/2016/03/08/20/03/flag-1244648_960_720.jpg"
+                />
+                <Card.Body className="usertitleLook">
+                  <Card.Title>{item.username}</Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
           ))}
         </div>
       ) : (
